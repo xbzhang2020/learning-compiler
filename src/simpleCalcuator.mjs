6c75a1fb11b481637694b817cfc3f3b8325ca040
zhangxingbin <xbzhang2020@icloud.com>
2022-07-10 22:59:54 +0800

    feat: 基础表达式

--- src/simpleCalcuator.mjs
+++ src/simpleCalcuator.mjs
@@ -6,49 +6,86 @@ import { ASTNode, ASTNodeType } from './ast.mjs'
 import { TokenType } from './token.mjs'
 
 export class SimpleCalcuator {
-  constructor() {
-    this.node = null
-    // this.tokens = null // token 序列读取器
-  }
+  constructor() {}
 
   initDelcareaction(tokens) {}
 
+  /**
+   * 解析加法表达式
+   * @param {*} tokens token序列读取器
+   * @returns ASTNode
+   */
   additive(tokens) {
-    return null
-  }
-
-  multiplicative(tokens) {
-    let node = null
-    let token = tokens.peek() // 当前token
-    if (token) {
-      if (token.type === TokenType.IntLiteral) {
-        token = tokens.read()
-        node = new ASTNode(ASTNodeType.IntLiteral, token.text)
-
-        token = tokens.peek()
-        if (token && token.type === TokenType.Star) {
-          token = tokens.read()
-          const child1 = node
-          const child2 = this.multiplicative(tokens)
-          if (child2) {
-            node = new ASTNode(ASTNodeType.Multiplicative, token.text)
-            node.children.push(child1, child2)
-          } else {
-            throw new Error(
-              'invalid multiplicative expression, expecting the right part.'
-            )
-          }
-        }
+    let node = this.multiplicative(tokens)
+    let token = tokens.peek() // 当前 token
+    if (node && token && token.type === TokenType.Plus) {
+      token = tokens.read()
+      const child1 = node
+      const child2 = this.additive(tokens)
+      if (child2) {
+        node = new ASTNode(ASTNodeType.Additive, token.text)
+        node.children.push(child1, child2)
+      } else {
+        throw new Error(
+          'invalid additive expression, expecting the right part.'
+        )
       }
     }
 
     return node
   }
 
+  /**
+   * 解析乘法表达式
+   * @param {*} tokens token序列读取器
+   * @returns ASTNode
+   */
+  multiplicative(tokens) {
+    let node = this.primary(tokens)
+    let token = tokens.peek() // 当前token
+
+    if (node && token && token.type === TokenType.Star) {
+      token = tokens.read()
+      const child1 = node
+      const child2 = this.multiplicative(tokens)
+      if (child2) {
+        node = new ASTNode(ASTNodeType.Multiplicative, token.text)
+        node.children.push(child1, child2)
+      } else {
+        throw new Error(
+          'invalid multiplicative expression, expecting the right part.'
+        )
+      }
+    }
+
+    return node
+  }
+
+  /**
+   * 解析基础表达式
+   * @param {*} tokens token序列读取器
+   * @returns ASTNode
+   */
+  primary(tokens) {
+    let node = null
+    let token = tokens.peek()
+
+    if (token) {
+      if (token.type === TokenType.IntLiteral) {
+        token = tokens.read()
+        node = new ASTNode(ASTNodeType.IntLiteral, token.text)
+      } else if (token.type === TokenType.Identifier) {
+        token = tokens.read()
+        node = new ASTNode(ASTNodeType.Identifier, token.text)
+      }
+    }
+    return node
+  }
+
   parse(code) {
     const lexer = new SimpleLexer()
     this.tokens = new TokenReader(lexer.tokenize(code))
-    const res = this.multiplicative(this.tokens)
+    const res = this.additive(this.tokens)
     if (res) {
       res.traversal((node) => console.log(node.text))
     }
@@ -61,7 +98,7 @@ export class SimpleCalcuator {
  */
 function test1() {
   const parser = new SimpleCalcuator()
-  parser.parse('2 * 3 * 4')
+  parser.parse('2 + 3 * 4')
 }
 
 test1()
