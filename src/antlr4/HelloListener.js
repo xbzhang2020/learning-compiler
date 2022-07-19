a724a6431783655f2313a52506f120a588dae72f
zhangxingbin <xbzhang2020@icloud.com>
2022-07-20 07:19:05 +0800

    feat: antlr4

--- src/antlr4/HelloListener.js
+++ src/antlr4/HelloListener.js
@@ -1,18 +1,12 @@
 // Generated from Hello.g4 by ANTLR 4.10.1
 // jshint ignore: start
-import antlr4 from 'antlr4';
+import antlr4 from 'antlr4'
 
 // This class defines a complete listener for a parse tree produced by HelloParser.
 export default class HelloListener extends antlr4.tree.ParseTreeListener {
+  // Enter a parse tree produced by HelloParser#r.
+  enterR(ctx) {}
 
-	// Enter a parse tree produced by HelloParser#r.
-	enterR(ctx) {
-	}
-
-	// Exit a parse tree produced by HelloParser#r.
-	exitR(ctx) {
-	}
-
-
-
+  // Exit a parse tree produced by HelloParser#r.
+  exitR(ctx) {}
 }
