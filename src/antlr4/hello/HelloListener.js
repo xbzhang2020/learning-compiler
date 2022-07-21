26e1e05399c8dff2852294e223da548198dbb78d
zhangxingbin <xbzhang2020@icloud.com>
2022-07-22 07:28:12 +0800

    feat: antlr4

--- src/antlr4/hello/HelloListener.js
+++ src/antlr4/hello/HelloListener.js
@@ -8,5 +8,8 @@ export default class HelloListener extends antlr4.tree.ParseTreeListener {
   enterR(ctx) {}
 
   // Exit a parse tree produced by HelloParser#r.
-  exitR(ctx) {}
+  exitR(ctx) {
+    const value = ctx.getChild(1).getText()
+    console.log(`hello, ${value}`)
+  }
 }
