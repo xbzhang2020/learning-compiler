170af488cbae4614ec79885ba06d033d844c9ee0
zhangxingbin <xbzhang2020@icloud.com>
2022-07-21 21:17:07 +0800

    feat: 计算器

--- src/antlr4/run.js
+++ src/antlr4/run.js
@@ -3,7 +3,7 @@ import MyGrammarLexer from './HelloLexer.js'
 import MyGrammarParser from './HelloParser.js'
 import MyGrammarListener from './HelloListener.js'
 
-const input = 'hello world'
+const input = 'hello world hello text'
 const chars = new antlr4.InputStream(input)
 const lexer = new MyGrammarLexer(chars)
 const tokens = new antlr4.CommonTokenStream(lexer)
@@ -11,7 +11,7 @@ const parser = new MyGrammarParser(tokens)
 parser.buildParseTrees = true
 
 const tree = parser.r()
-
+console.log(tree.toStringTree())
 // 监听器
-const listener = new MyGrammarListener()
-antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree)
+// const listener = new MyGrammarListener()
+// antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree)
