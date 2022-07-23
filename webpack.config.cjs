7cd22ed2c87c189542966e05a68a8907561c88f5
zhangxingbin <xbzhang2020@icloud.com>
2022-07-23 08:04:58 +0800

    feat: 简易计算器

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,13 +1,48 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const fs = require('fs')
+
+// 获取多页面路径
+const pagePaths = ['calc']
+
+// 获取多页面路径
+// function getPagePaths() {
+//   try {
+//     const files = fs.readdirSync('./src/pages')
+//     pagePaths.push(...files)
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
+
+// 获取多页面的入口信息
+function getPagesEntryInfo(paths = []) {
+  return paths.reduce((res, path) => {
+    res[path] = `./src/pages/${path}/index.js`
+    return res
+  }, {})
+}
+
+// 获取多页面的html-webpack-plugins配置
+function getPagesHtmlWebpackPluginInfo(paths = []) {
+  return paths.map(
+    (path) =>
+      new HtmlWebpackPlugin({
+        template: `./src/pages/${path}/index.html`,
+        chunks: [path],
+        filename: `${path}.html`,
+      })
+  )
+}
 
 module.exports = {
   mode: 'production',
   entry: {
     index: './src/index.js',
+    ...getPagesEntryInfo(pagePaths),
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'docs'),
   },
   module: {
@@ -28,5 +63,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    ...getPagesHtmlWebpackPluginInfo(pagePaths),
   ],
 }
